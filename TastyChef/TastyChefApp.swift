e329e7a967f22890ef34e5d2e8ebf95884882a31
AhmedHamza667 <hamzaa667788@gmail.com>
2025-03-24 14:16:06 -0400

    Initial Commit

---
+++ TastyChef/TastyChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyChefApp.swift
+//  TastyChef
+//
+//  Created by Ahmed Hamza on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TastyChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
